da81e7306ea1d397f009b437499759e68bd0ac0c
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-14 10:22:34 -0700

    more layout of the json file

--- .github/actions/convertIssue/parse-issue-body.js
+++ .github/actions/convertIssue/parse-issue-body.js
@@ -9,6 +9,60 @@ function normalizeNewLines(str) {
   return str.replace(/\r\n/g, "\n");
 }
 
+function splitList(str){
+  let list = str.split(',');
+  for(let i = 0; i < list.length; i++) {
+    list[i] = list[i].strip();
+  }
+}
+
+function getSurveyInfo(dataObject) {
+  let surveyInfo = {};
+
+  //demographics survey settings
+  if(dataObject[survey_form_path]) {
+    surveyInfo.surveys.UserProfileSurvey = {
+      "formPath": "json/demo-survey-v2.json",
+      "version": 1,
+      "compatibleWith": 1,
+      "dataKey": "manual/demographic_survey",
+      "labelTemplate": {
+        "en": "Answered",
+        "es": "Contestada"
+      }
+    }
+  } else {
+    surveyInfo.surveys.UserProfileSurvey = {
+      "formPath": 'https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/survey_resources/' + dataObject[url_abbreviation] + '/' + dataObject[custom_dem_survey_path],
+      "version": 1,
+      "compatibleWith": 1,
+      "dataKey": "manual/demographic_survey",
+      "labelTemplate": {
+        "en": dataObject[labelTemplate_lang1].split('-')[1].strip(),
+        "es": dataObject[labelTemplate_lang2].split('-')[1].strip()
+      }
+    }
+  }
+
+  //labeling options
+  if(dataObject[label_form_path]){
+    surveyInfo[trip-labels] = "MULTILABEL";
+  } else if (dataObject[label_options] && dataObject[label_options] != '') {
+    surveyInfo[trip-labels] = "MULTILABEL"; //label_options goes outside of this?
+  } else {
+    //TODO determine proceedure for custom label surveys
+    surveyInfo[trip-labels] = "ENKETO";
+  }
+}
+
+/**
+ * TODO: ensure good error messaging so deployers can fix bugs
+ * fields are from the issue template
+ * bodyData is from the filled out issue
+ * @param {*} githubIssueTemplateFile 
+ * @param {*} body 
+ * @returns 
+ */
 export async function parseIssueBody(githubIssueTemplateFile, body) {
   let issueTemplate = await readFile(githubIssueTemplateFile, "utf8");
   let githubFormData = yaml.load(issueTemplate);
@@ -25,23 +79,15 @@ export async function parseIssueBody(githubIssueTemplateFile, body) {
     return entry === "_No response_" ? "" : entry;
   });
 
+  //map fields and entries to an object, then we map that 
   let returnObject = {};
   for(let j = 0, k = bodyData.length; j<k; j++) {
+    //skip matching if the field does not exist
     if(!fields[j]) {
       continue;
     }
 
     let entry = bodyData[j];
-    let attributes = fields[j] && fields[j].attributes || {};
-    let fieldLabel = attributes.label || "";
-    let fieldDescription =  attributes.description || "";
-
-    // Only supports a single checkbox (for now)
-    if(fields[j].type === "checkboxes") {
-      entry = removeNewLines(entry);
-      // Convert to Boolean
-      entry = entry.startsWith("- [X]");
-    }
 
     returnObject[fields[j].id] = entry;
   }
@@ -49,23 +95,54 @@ export async function parseIssueBody(githubIssueTemplateFile, body) {
   console.log( { fields, bodyData, returnObject } );
 
   let configObject = {};
-  configObject['version'] = 1;
-  configObject['ts'] = Date.now();
+  configObject[version] = 1;
+  configObject[ts] = Date.now();
 
-  let connect_url = 'https://' + returnObject['url_abbreviation'] + '-openpath.nrel.gov/api/';
-  configObject['server'] = {connectURL: connect_url, aggregate_call_auth: 'user_only'}; //TODO check options for call + add to form?
+  let connect_url = 'https://' + returnObject[url_abbreviation] + '-openpath.nrel.gov/api/';
+  configObject[server] = {connectURL: connect_url, aggregate_call_auth: 'user_only'}; //TODO check options for call + add to form?
 
-  let subgroups = returnObject['subgroups'].split(',');
-  configObject['opcode'] = {autogen: returnObject['autogen'], subgroups: subgroups};
+  let subgroups = returnObject[subgroups].split(',');
+  configObject['opcode'] = {autogen: returnObject[autogen], subgroups: subgroups};
 
   configObject['intro'] = {
-    program_or_study: returnObject['program_or_study'],
-    start_month: returnObject['start'].split('/')[0],
-    start_year: returnObject['start'].split('/')[1],
+    program_or_study: returnObject[program_or_study],
+    start_month: returnObject[start].split( '/')[0],
+    start_year: returnObject[start].split('/')[1],
     // mode_studied: , //TODO - add this to the form and find a way to maintain it as optional
-    program_admin_contact: returnObject['program_admin_contact'],
-    deployment_partner_name: returnObject['deployment_partner_name_lang1']
+    program_admin_contact: returnObject[program_admin_contact],
+    deployment_partner_name: returnObject[deployment_partner_name_lang1]
   };
+
+  configObject['survey_info'] = getSurveyInfo(returnObject);
+  if(returnObject[label_options]) {
+    configObject[label_options] = 'https://raw.githubusercontent.com/e-mission/nrel-openpath-deploy-configs/main/label_options/' + returnObject[label_options];
+  }
+
+  configObject['display_config'] = { use_imperial: returnObject[use_imperial] };
+  configObject['metrics'] = { include_test_users: returnObject[include_test_users] };
+  configObject['profile_controls'] = { support_upload: false, trip_end_notification: returnObject[trip_end_notification] };
+
+  configObject['admin_dashboard'] = {
+    overview_users: returnObject[overview_users],
+    overview_active_users: returnObject[overview_active_users],
+    overview_trips: returnObject[overview_trips],
+    overview_signup_trends: returnObject[overview_signup_trends],
+    overview_trips_trend: returnObject[overview_trips_trend],
+    data_uuids: returnObject[data_uuids],
+    data_trips: returnObject[data_trips],
+    data_trips_columns_exclude: splitList(returnObject[data_trips_columns_exclude]),
+    additional_trip_columns: splitList(returnObject[additional_trip_columns]),
+    data_uuids_columns_exclude: splitList(returnObject[data_uuids_columns_exclude]),
+    token_generate: returnObject[token_generate],
+    //TODO: will this ever NOT be nrelop?
+    token_prefix: "nrelop",
+    map_heatmap: returnObject[map_heatmap],
+    map_bubble: returnObject[map_bubble],
+    map_trip_lines: returnObject[map_trip_lines],
+    push_send: returnObject[push_send],
+    options_uuids: returnObject[options_uuids],
+    options_emails: returnObject[options_emails]
+  }
 
   console.log( { configObject } );
   return configObject;
