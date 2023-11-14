64983552e25e2d965b79a6ee5c2a4f941e18ff01
Abby Wheelis <abby.wheelis@centre.edu>
2023-11-13 17:40:39 -0700

    custom parsing
    create a JSON file that is not "flat"

--- .github/actions/convertIssue/parse-issue-body.js
+++ .github/actions/convertIssue/parse-issue-body.js
@@ -25,41 +25,47 @@ export async function parseIssueBody(githubIssueTemplateFile, body) {
     return entry === "_No response_" ? "" : entry;
   });
 
-  // console.log( { fields, bodyData } );
+  console.log( { fields, bodyData } );
 
-  let returnObject = {};
+  let configObject = {};
   for(let j = 0, k = bodyData.length; j<k; j++) {
-    if(!fields[j]) {
-      continue;
-    }
+    configObject['version'] = 1;
+    configObject['ts'] = Date.now();
 
-    let entry = bodyData[j];
-    let attributes = fields[j] && fields[j].attributes || {};
-    // let fieldLabel = attributes.label || "";
-    let fieldDescription =  attributes.description || "";
+    let connect_url = 'https://' + bodyData['url_abbreviation'] + '-openpath.nrel.gov/api/';
+    configObject['server'] = {connectURL: connect_url, aggregate_call_auth: 'user_only'}; //TODO check options for call + add to form?
 
-    // Clean up GitHub or Twitter usernames (strip out commas, convert to array, remove @ prefixes)
-    // if(fieldDescription.includes("[parser:usernames]")) {
-    //   entry = cleanupUsernames(removeNewLines(entry));
+    let subgroups = bodyData['subgroups'].split(',');
+    configObject['opcode'] = {autogen: bodyData['autogen'], subgroups: subgroups};
+
+    configObject['intro'] = {
+      program_or_study: bodyData['program_or_study'],
+      start_month: bodyData['start'].split('/')[0],
+      start_year: bodyData['start'].split('/')[1],
+      // mode_studied: , //TODO - add this to the form and find a way to maintain it as optional
+      program_admin_contact: bodyData['program_admin_contact'],
+      deployment_partner_name: bodyData['deployment_partner_name_lang1']
+    };
+
+    // if(!fields[j]) {
+    //   continue;
     // }
 
-    // Normalize urls (add protocol, follow redirects)
-    // if(fieldDescription.includes("[parser:url]")) {
+    // let entry = bodyData[j];
+    // let attributes = fields[j] && fields[j].attributes || {};
+    // // let fieldLabel = attributes.label || "";
+    // let fieldDescription =  attributes.description || "";
+
+    // // Only supports a single checkbox (for now)
+    // if(fields[j].type === "checkboxes") {
     //   entry = removeNewLines(entry);
-    //   console.log( "About to cleanup URL: ", { entry } );
-    //   entry = await cleanupUrl(entry);
+    //   // Convert to Boolean
+    //   entry = entry.startsWith("- [X]");
     // }
 
-    // Only supports a single checkbox (for now)
-    if(fields[j].type === "checkboxes") {
-      entry = removeNewLines(entry);
-      // Convert to Boolean
-      entry = entry.startsWith("- [X]");
-    }
-
-    returnObject[fields[j].id] = entry;
+    // returnObject[fields[j].id] = entry;
   }
 
-  console.log( { returnObject } );
-  return returnObject;
+  console.log( { configObject } );
+  return configObject;
 }
